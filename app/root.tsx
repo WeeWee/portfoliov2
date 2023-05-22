26190e1f71ff64e5d9f252355230e61a94a7e89d
adamthehotman <adam.kindberg@elev.ga.lbs.se>
2023-05-22 14:27:52 +0200

    changed seo

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, V2_MetaFunction } from "@remix-run/node";
 import {
 	Links,
 	LiveReload,
@@ -15,7 +15,25 @@ export const links: LinksFunction = () => [
 		? [{ rel: "stylesheet", href: cssBundleHref }]
 		: [{ rel: "stylesheet", href: stylesheet }]),
 ];
-
+export const meta: V2_MetaFunction = () => {
+	const title = "Adam Kindberg's Portfolio";
+	const description = "Adam Kindberg's portfolio website";
+	return [
+		{ title },
+		{ name: "description", content: description },
+		{ name: "twitter:description", content: description },
+		{ property: "og:url", content: "https://adamkindberg.com" },
+		{ property: "og:type", content: "website" },
+		{ property: "og:title", content: title },
+		{ property: "og:description", content: description },
+		{ property: "og:image", content: "https://i.imgur.com/IhFrz4E.png" },
+		{ name: "twitter:card", content: "summary_large_image" },
+		{ name: "twitter:url", content: "https://adamkindberg.com" },
+		{ name: "twitter:title", content: title },
+		{ name: "twitter:description", content: description },
+		{ name: "twitter:image", content: "https://i.imgur.com/IhFrz4E.png" },
+	];
+};
 export default function App() {
 	return (
 		<html lang="en" className="scroll-smooth">
