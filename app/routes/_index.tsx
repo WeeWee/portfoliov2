f299634426ff963818b8bffac00a35606748ae5f
adamthehotman <adam.kindberg@elev.ga.lbs.se>
2023-05-22 14:29:37 +0200

    changed seo

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,4 @@
-import type { V2_MetaFunction } from "@remix-run/node";
-import { motion } from "framer-motion";
 import { Hero, Navbar, Projects, Skills } from "~/components";
-
-export const meta: V2_MetaFunction = () => {
-	return [{ title: "Adam Kindberg Portfolio" }];
-};
 
 export default function Index() {
 	return (
