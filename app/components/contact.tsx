e7fbed1babd44fda0ce458aa6e7fc0deeeacbca6
IndieAK <kindbergadam@gmail.com>
2023-05-25 16:52:51 +0200

    fixed contact

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -1,16 +1,25 @@
 import { motion } from "framer-motion";
 export function Contact() {
+  const contactAnimation = {
+    hidden: {
+      opacity: 0,
+      y: 50,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 4,
+        ease: "easeInOut",
+        duration: 0.5,
+      },
+    },
+  };
   return (
     <motion.div
-      initial={{ opacity: 0, x: -50 }}
-      animate={{ opacity: 1, x: 0 }}
-      transition={{
-        delay: 4,
-        /* ease: "easeOut" */ type: "spring",
-        damping: 20,
-        stiffness: 300,
-        duration: 2.4,
-      }}
+      variants={contactAnimation}
+      initial="hidden"
+      animate="visible"
       className="mt-8 mb-4"
       id="contact"
     >
